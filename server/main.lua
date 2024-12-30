ad7cfa8571406ccaf8d59734fed676ad43f00fb2
XSCRIPTS <104461809+xscriptsdev@users.noreply.github.com>
2024-12-30 22:25:28 +0100

    Revert "ESX and QBCore support"

--- server/main.lua
+++ server/main.lua
@@ -1,44 +1,29 @@
-local Framework = nil
+ESX = exports['es_extended']:getSharedObject()
 
-if GetResourceState('qb-core') == 'started' then
-    Framework = exports['qb-core']:GetCoreObject()
-elseif GetResourceState('es_extended') == 'started' then
-    Framework = exports['es_extended']:getSharedObject()
-end
+RegisterServerEvent('x-lockpick:buyLockpick')
+AddEventHandler('x-lockpick:buyLockpick', function()
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local money = xPlayer.getMoney()
 
-RegisterServerEvent('x-lockpick:buyLockpick', function()
-    local src = source
-    if Framework then
-        if GetResourceState('qb-core') == 'started' then
-            local Player = Framework.Functions.GetPlayer(src)
-            if Player.Functions.RemoveMoney('cash', Config.LockpickPrice) then
-                Player.Functions.AddItem(Config.LockpickItem, 1)
-                TriggerClientEvent('ox_lib:notify', src, {title = Config.PurchaseTitle, description = Config.BoughtLockpick, type = 'success'})
-            else
-                TriggerClientEvent('ox_lib:notify', src, {title = Config.NotEnoughMoney, description = Config.NotEnoughMoneyDesc, type = 'error'})
-            end
-        elseif GetResourceState('es_extended') == 'started' then
-            local xPlayer = Framework.GetPlayerFromId(src)
-            if xPlayer.getMoney() >= Config.LockpickPrice then
-                xPlayer.removeMoney(Config.LockpickPrice)
-                xPlayer.addInventoryItem(Config.LockpickItem, 1)
-                TriggerClientEvent('ox_lib:notify', src, {title = Config.PurchaseTitle, description = Config.BoughtLockpick, type = 'success'})
-            else
-                TriggerClientEvent('ox_lib:notify', src, {title = Config.NotEnoughMoney, description = Config.NotEnoughMoneyDesc, type = 'error'})
-            end
-        end
+    if money >= Config.LockpickPrice then
+        xPlayer.removeMoney(Config.LockpickPrice)
+        xPlayer.addInventoryItem(Config.LockpickItem, 1)
+        TriggerClientEvent('ox_lib:notify', source, {title = Config.PurchaseTitle, description = Config.BoughtLockpick, type = 'success'})
+    else
+        TriggerClientEvent('ox_lib:notify', source, {title = Config.NotEnoughMoney, description = Config.NotEnoughMoneyDesc, type = 'error'})
     end
 end)
 
-RegisterServerEvent('x-lockpick:removeLockpick', function()
-    local src = source
-    if Framework then
-        if GetResourceState('qb-core') == 'started' then
-            local Player = Framework.Functions.GetPlayer(src)
-            Player.Functions.RemoveItem(Config.LockpickItem, 1)
-        elseif GetResourceState('es_extended') == 'started' then
-            local xPlayer = Framework.GetPlayerFromId(src)
-            xPlayer.removeInventoryItem(Config.LockpickItem, 1)
-        end
-    end
+ESX.RegisterServerCallback('x-lockpick:hasLockpick', function(source, cb)
+  local xPlayer = ESX.GetPlayerFromId(source)
+  local hasLockpick = xPlayer.getInventoryItem(Config.LockpickItem).count > 0
+
+  cb(hasLockpick)
+end)
+
+
+RegisterServerEvent('x-lockpick:removeLockpick')
+AddEventHandler('x-lockpick:removeLockpick', function()
+    local xPlayer = ESX.GetPlayerFromId(source)
+    xPlayer.removeInventoryItem(Config.LockpickItem, 1)
 end)
